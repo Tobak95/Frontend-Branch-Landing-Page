badf62d9a32ffb01175ffe7a5cc7f7fbfe5e2542
AA-fullstackdev <adeleke_adeyemi@outlook.com>
2025-09-08 08:32:16 +0100

    styles

--- src/component/AboutMe.jsx
+++ src/component/AboutMe.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import Group from "../assets/Group 62.png"
 
 const AboutMe = () => {
   return (
     <>
-      <main>
+      <main className="layout bg-[#222831]">
+        <article className="flex ">
+
         {/* section for About me context */}
-        <section>
-          <h1>About me</h1>
-          <p>
+        <section className="text-white w-1/2">
+          <h1 className="text-[64px]  font-Poppins leading-[100%] tracking-[0%] ">About <span className="text-[#00ADB5]">me</span></h1>
+          <p className="font-Poppins leading-[100%] tracking-[0%]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
             vulputate libero et velit interdum, ac aliquet odio mattis. Class
             aptent taciti sociosqu ad litora torquent per conubia nostra, per
@@ -16,9 +19,10 @@ const AboutMe = () => {
           </p>
         </section>
         {/* Section for About Me Illustration Image */}
-        <section>
-          
+        <section id="illustration" >
+          <figure><img src={Group} alt="ilustration image " /></figure>
         </section>
+        </article>
       </main>
     </>
   );
